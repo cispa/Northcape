71a7411e2dc527af21b4cc6d9c50e77d39202811
Eric Ackermann <eric.ackermann@cispa.de>
2026-06-16 18:46:11 +0200

    Fix incompatibility between OpenOCD and Zephyr on GenesysII

--- software/include/northcape-zephyr-stack/boards/openhwgroup/cv64a6_genesysII_nocape/board.cmake
+++ software/include/northcape-zephyr-stack/boards/openhwgroup/cv64a6_genesysII_nocape/board.cmake
@@ -4,6 +4,6 @@ board_runner_args(openocd "--use-elf")
 board_runner_args(openocd "--verify")
 # board_runner_args(openocd "--cmd-pre-init=\"adapter driver ftdi\"")
 # board_runner_args(openocd "--cmd-pre-init=\"tcl_port 6666\"")
-board_runner_args(openocd "--cmd-pre-init=riscv.cpu configure -rtos none -work-area-phys 0x90000000 -work-area-size 16780000")
+board_runner_args(openocd "--cmd-pre-init=riscv.cpu configure -work-area-phys 0x90000000 -work-area-size 16780000")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
